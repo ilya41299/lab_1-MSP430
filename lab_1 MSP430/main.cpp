b9a4cf8cf06ad39ec1482479107e1dfe7031ed41
RedFox <lobusovis@student.bmstu.ru>
2020-11-11 02:53:29 +0300

    make all boxs

--- lab_1 MSP430/main.cpp
+++ lab_1 MSP430/main.cpp
@@ -36,7 +36,7 @@
 uint8_t P_box[8] = { 0x4, 0x0, 0x6, 0x1, 0x3, 0x5, 0x7, 0x2 };
 uint8_t S_box_high[16] = { 0xF, 0xD, 0x0, 0x6, 0x9, 0x5, 0xA, 0x1, 0x2, 0xB, 0x3, 0x8, 0xC, 0x7, 0xE, 0x4 };
 uint8_t S_box_low[16] = { 0x4, 0xF, 0x3, 0x6, 0xB, 0xC, 0x0, 0x9, 0x1, 0xA, 0x8, 0x5, 0xD, 0x7, 0xE, 0x2 };
-uint8_t LFSR[4] = { 0x8, 0x0, 0xA, 0xE };
+uint16_t LFSR = 0b0100010100000001;
 uint8_t shift_n = 0x7;
 
 
@@ -50,7 +50,7 @@ uint8_t counter_iter = 0;               //счетчик итераций
 uint8_t read_encr = 0;
 
 //переменная для выбора режима чтения байтов при шифровании 0x00000001 - show text, 0x00000010 - show key
-uint8_t text_key = 0;
+uint8_t text_key = 0b00000001;
 
 //массив для ССИ {0. 1. 2. 3. 4. 5. 6. 7. 8.}, для исбавления от точки будем применять маскирование
 uint8_t HG[10] = { 0b11111101, 0b11100000, 0b11010111, 0b11110110, 0b11101010, 0b10111110, 0b10111111, 0b11110000, 0b11111111 };
@@ -73,52 +73,73 @@ void indication(uint8_t data, uint8_t HG)
         P2OUT = LEDS_EN;        //разрешаем запись в регистр LEDS
     P2OUT &= ~DC_EN;            //отключаем дешифратор    
     P1DIR = 0b00000000;         //конфигурируем P1 на ввод данных с кнопок
+    P2OUT = BTNS_EN;            //включаем регистр кнопок
 }
 
 //функция применения P-box. i - номер байта, text_or_key - 0 (text), 1 (key)
-void make_p_box(uint8_t i, uint8_t text_or_key)
+void make_p_box(uint8_t *data)
 {
-    uint8_t temp_value = 0;
-   
-    if (text_or_key)
-    {
-        temp_value = key[i];
-        key[i] = 0;
-    }
-    else 
-    {
-        temp_value = text[i];
-        text[i] = 0;
-    }
-    temp_value = (
-            ((temp_value & 0b00000001) << P_box[0]) | (((temp_value & 0b00000010) >> 1) << P_box[1]) |
-            (((temp_value & 0b00000100) >> 2) << P_box[2]) | (((temp_value & 0b00001000) >> 3) << P_box[3]) |
-            (((temp_value & 0b00010000) >> 4) << P_box[4]) | (((temp_value & 0b00100000) >> 5) << P_box[5]) |
-            (((temp_value & 0b01000000) >> 6) << P_box[6]) | (((temp_value & 0b10000000) >> 7) << P_box[7])
+    *data = (
+            ((*data & 0b00000001) << P_box[0]) | (((*data & 0b00000010) >> 1) << P_box[1]) |
+            (((*data & 0b00000100) >> 2) << P_box[2]) | (((*data & 0b00001000) >> 3) << P_box[3]) |
+            (((*data & 0b00010000) >> 4) << P_box[4]) | (((*data & 0b00100000) >> 5) << P_box[5]) |
+            (((*data & 0b01000000) >> 6) << P_box[6]) | (((*data & 0b10000000) >> 7) << P_box[7])
         );
-    if (text_or_key)
+}
+
+
+void make_s_box(uint8_t& data)
+{
+    uint8_t low = data & 0b00001111;   //выделяем младший полубайт
+    uint8_t high = data >> 4;          //выделяем старший полубайт
+    low = S_box_low[low];               //применяем подстанувку к младшему полубайту
+    high = S_box_high[high];             //применяем подстанувку к старшему полубайту
+    data = (high << 4) | low;          //записываем результат применения Sbox
+}
+
+void make_LFSR(uint8_t& high, uint8_t& low)
+{
+    uint16_t reg = (high << 8) | low;
+    uint16_t sum = reg & LFSR;
+
+    uint16_t new_bit_15 = 0;
+
+    while (sum)
     {
-        key[i] = temp_value;
+        new_bit_15 ^= sum & 0b00000001;
+        sum = sum >> 1;
     }
-    else
-        text[i] = temp_value;
+    reg = reg >> 1;
+    low = reg & 0b0000000011111111;
+    high = (reg >> 8) | (new_bit_15 << 7);
 }
 
-
-void make_s_box()
+//функция циклического сдвига shift_value: 0 - text, 1 - key
+void make_shift(uint8_t(&shift_value)[4])
 {
+    uint32_t shift_data = (shift_value[3] << 24) | (shift_value[2] << 16) | (shift_value[1] << 8) | shift_value[0];
+    uint8_t n = shift_n;
+    uint8_t new_bit_0 = 0;
 
+    while (n--)
+    {
+        new_bit_0 = !!(shift_data & 0x80000000);
+        shift_data = shift_data << 1;
+        shift_data |= new_bit_0;
+    }
+    shift_value[0] = shift_data & 0x000000FF;
+    shift_value[1] = (shift_data & 0x0000FF00) >> 8;
+    shift_value[2] = (shift_data & 0x00FF0000) >> 16;
+    shift_value[3] = (shift_data & 0xFF000000) >> 24;
 }
 
-void make_LFSR()
-{
-
-}
 
 // обработчик прерываний порта P1
 #pragma vector=PORT1_VECTOR
 __interrupt void PORT1_ISR(void)
 {
+    //проверка, для выхода из режима индикации байта данных или ключа, если не нажади кнопку байта
+    
     //при появлении флага
     if ((P1IFG & WRITE_BYTE) && (read_encr & READ_TEXT))
     {
@@ -155,35 +176,61 @@ __interrupt void PORT1_ISR(void)
         }
         else 
             indication(HG[counter_iter] & 0b01111111, 1);    //выводим на ССИ HG1
-        P2OUT = BTNS_EN;                                     //разрешаем запись в регистр кнопок
     }
     //для режима шифрования
     //выполить следующую итерацию шифрования
-    else if (P1IFG & NEXT_STEP)
+    else 
     {
-        text_key = 0b00000000;
+        if ((text_key & (SHOW_KEY_ACTIVE | SHOW_TEXT_ACTIVE)) && !(P1IFG & (BYTE_0 | BYTE_1 | BYTE_2 | BYTE_3)))
+        {
+            text_key = 0b00000001;
+            indication(0b00000000, 1); //отключаем индикацию ССИ HG1
+        }
+        if (P1IFG & NEXT_STEP)
+        {
+            //uint8_t i = 0;
+        }
+        //шифрование с начала
+        else if (P1IFG & ENCRYPT_AGAIN)
+        {
+            text_key = 0b00000000;
+        }
+        //активация индикации шифртекста
+        else if (P1IFG & SHOW_TEXT)
+        {
+            text_key = 0b00000001;
+            
+        }
+        //активация индикации ключа
+        else if (P1IFG & SHOW_KEY)
+        {
+            text_key = 0b00000010;
+        }
+        //индикация байта данных или ключа
+        else if ((P1IFG & (BYTE_3 | BYTE_2 | BYTE_1 | BYTE_0)) && (text_key & (SHOW_TEXT_ACTIVE | SHOW_KEY_ACTIVE)))
+        {
+            uint8_t i_byte = 0;
+            if (P1IFG & BYTE_0)
+                i_byte = 0;
+            else if (P1IFG & BYTE_1)
+                i_byte = 1;
+            else if (P1IFG & BYTE_2)
+                i_byte = 2;
+            else i_byte = 3;
+            if (text_key & SHOW_KEY_ACTIVE)
+            {
+                indication(key[i_byte], 3);
+                indication((HG[i_byte] & 0b01111111), 1);
+            }
+            else if (text_key & SHOW_TEXT_ACTIVE)
+            {
+                indication(text[i_byte], 3);
+                indication((HG[i_byte] & 0b01111111), 1);
+            }
+        }
     }
-    //шифрование с начала
-    else if (P1IFG & ENCRYPT_AGAIN)
-    {
-        text_key = 0b00000000;
-    }
-    //активация индикации шифртекста
-    else if (P1IFG & SHOW_TEXT)
-    {
-        text_key = 0b00000001;
-    }
-    //активация индикации ключа
-    else if (P1IFG & SHOW_KEY)
-    {
-        text_key = 0b00000010;
-    }
-    //индикация байта данных или ключа
-    else if ((P1IFG & (BYTE_3 | BYTE_2 | BYTE_1 | BYTE_0)) && (text_key & (SHOW_TEXT_ACTIVE | SHOW_KEY_ACTIVE)))
-    {
-
-    }
-    //тут БУДЕТ ОПИСАНИЕ ДЛЯ ПРЕРЫВАНИЙ В РЕЖИМЕ ШИФРОВАНИЯ
+    
+    
     
     P1IFG = 0b00000000;      // опускаем флаг прерывания
 }
@@ -197,30 +244,22 @@ void init()
     P2DIR |= 0b11101000;        //конфигурируем P2 на вывод для управления защелками
         
     //очищаем тексты и ключи
-    init_key[0] = 0;
-    init_key[1] = 0;
-    init_key[2] = 0;
-    init_key[3] = 0;
-
-    init_text[0] = 0;
-    init_text[1] = 0;
-    init_text[2] = 0;
-    init_text[3] = 0;
-
-    key[0] = 0;
-    key[1] = 0;
-    key[2] = 0;
-    key[3] = 0;
-
-    text[0] = 0;
-    text[1] = 0;
-    text[2] = 0;
-    text[3] = 0;
-
+    uint8_t i = 0;
+    while (i++ < 4) 
+    {
+        init_key[i] = 0;
+        init_text[0] = 0;
+        key[i] = 0;
+        text[i] = 0;
+        //i++;
+    }
+  
     //выводим на индикатор счетчика "0."
     indication(HG[counter_iter], 0);
     //выводим на индикатор данных "0."
     indication(HG[counter_iter], 1);
+    //очищаем светодиоды
+    indication(0b00000000, 3);
     //переходим режим ввода данных
     read_encr = 0b00000001;
  }
@@ -230,29 +269,25 @@ void read_text_and_key()
     //подготавливаем микроконтроллер к чтению данных
     P1DIR &= 0b00000000;        //конфигурируем P1 на ввод для чтения
     read_encr = 0b00000001;     //устанавливаем флаг чтения
-    P1IES &= ~(WRITE_BYTE);     //устанавливаем прерывание по положительному фронту
+    P1IES |= WRITE_BYTE;     //устанавливаем прерывание по положительному фронту
     P1IFG &= ~WRITE_BYTE;       //опускаем флаг прерывания
     P1IE |= WRITE_BYTE;         //разрешаем прерывание на ножке с кнопкой SB1
-    __enable_interrupt();       //разрешаем работу прерываний
-    P2OUT = BTNS_EN;            //включаем регистр кнопок    
+    __enable_interrupt();       //разрешаем работу прерываний 
 }
 
 void encrypt() 
 {
     //инициализируем ключ и текст для шифрования
-    text[0] = init_text[0];
-    text[1] = init_text[1];
-    text[2] = init_text[2];
-    text[3] = init_text[3];
-    key[0] = init_key[0];
-    key[1] = init_key[1];
-    key[2] = init_key[2];
-    key[3] = init_key[3];
+    uint8_t i = 0;
+    while (i++ < 4) 
+    {
+        text[i] = init_text[i];
+        key[i] = init_key[i];
+    }
     P1DIR &= 0b00000000;        //конфигурируем P1 на ввод для чтения
-    P1IES &= ~(0b11111111);     //устанавливаем прерывание по положительному фронту
+    P1IES |= 0b11111111;        //устанавливаем прерывание по положительному фронту
     P1IFG &= ~0b11111111;       //опускаем флаг прерывания
     P1IE |= 0b11111111;         //разрешаем прерывание на ножке с кнопкой SB1-SB8
-    P2OUT = BTNS_EN;            //включаем регистр кнопок    
 }
 
 int main(void)
